35ce6a0ff9d09ee58231c70d86c538763242cb7c
vikash8813 <vikashjangir2301@gmail.com>
2024-08-31 22:56:16 +0530

    updated react

--- vite-react/src/main.jsx
+++ vite-react/src/main.jsx
@@ -3,6 +3,11 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+const a = (
+    <div>
+        hello
+    </div>
+)
 createRoot(document.getElementById('root')).render(
-    <App />
+    App()
 )
